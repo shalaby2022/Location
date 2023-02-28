a757efc9c042bbca344927cddec80c3df9019f3e
shalaby2022 <ahmedshalaby201495@gmail.com>
2023-02-28 16:11:51 +0200

    creating live tracking component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Location from './src/components/Location';
 import Locations from './src/components/Locations';
+import Tracking from './src/components/Tracking';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       {/* <Location /> */}
-      <Locations />
+      {/* <Locations /> */}
+      <Tracking />
     </SafeAreaView>
   );
 };
